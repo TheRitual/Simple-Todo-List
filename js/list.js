7a786ffabd6db334e1738bc543e00cdaa1550b02
Ritual <dev@theritual.eu>
2021-06-20 12:33:18 +0200

    added done/undone button

--- js/list.js
+++ js/list.js
@@ -1,14 +1,25 @@
 {
 
     const tasks = [{
-            content: "Nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "Zjeść pizzę",
+            content: "Taning",
             done: true,
         },
+        {
+            content: "Eat Dinner",
+            done: false,
+        },
     ]
+
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
@@ -16,38 +27,56 @@
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? ' style="text-decoration: line-through"' : ''}>
+                <button class="js-done">
+                ${task.done ? '🥳' : '☹️'}
+                </button>
+                <button class="js-removeTaskButton">Remove</button>
                 ${task.content}
             </li>
             `
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((doneButton, taskIndex) => {
+            doneButton.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            });
+        });
+
+        const removeButtons = document.querySelectorAll(".js-removeTaskButton");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+    }
+
+    const addNewTask = (newTaskContent) => {
+        if (newTaskContent === "") {
+            return;
+        }
+
+        tasks.push({
+            content: newTaskContent,
+        });
+        document.querySelector(".js-newTask").value = "";
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        addNewTask(newTaskContent);
     }
 
     const init = () => {
         render();
-
         const form = document.querySelector(".js-form");
-
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const taskField = document.querySelector(".js-newTask");
-
-            const newTaskContent = taskField.value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            }
-
-            tasks.push({
-                content: newTaskContent,
-            });
-
-            taskField.value = "";
-
-            render();
-        });
+        form.addEventListener("submit", onFormSubmit);
     }
 
     init();
