cd00f14cc76da69ab9028f11896baa5635c07a21
Ritual <dev@theritual.eu>
2021-08-24 21:25:13 +0200

    moved and adjusted vanilla part to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+const tasks = [
+  {id : 1, content : "Przejść na Reacta", done : false},
+  {id : 2, content : "Zjeść obiad", done : true},
+]
+
+const hideDoneTasks = false;  
 
 function App() {
   return (
-    <div>
-        Dzień Dobry
-    </div>
+    <main className="main">
+        <header>
+            <h1>Task List</h1>
+        </header>
+
+
+        <section className="section">
+            <h2 className="section__header">Add a new Task</h2>
+            <div className="section__body">
+                <form>
+                    <input className="form__input" placeholder="What to do?" />
+                    <button className="form__button">Add Task</button>
+                </form>
+            </div>
+        </section>
+
+        <section className="section">
+            <h2 className="section__header">List of Tasks</h2>
+            <div className="section__body">
+                <ul className="tasks"></ul>
+            </div>
+        </section>
+    </main>
   );
 }
 
