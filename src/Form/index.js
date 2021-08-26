a592cbfa0678fa0c52cfe3840d6fc254e3bec1bc
Ritual <dev@theritual.eu>
2021-08-26 21:11:17 +0200

    form js updated

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Form = () => (
-    <form>
+    <form className="form">
         <input className="form__input" placeholder="What to do?" />
         <button className="form__button">Add Task</button>
     </form>
