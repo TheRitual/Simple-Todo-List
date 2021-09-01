eedcdadf74110e318dc28cb572126152dac711b5
Ritual <dev@theritual.eu>
2021-09-01 15:57:19 +0200

    added styled components to Section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import { StyledSection, SectionBody, SectionHeader, SectionTitle } from "./style";
 
-const Section = ({title, body, extraHeaderContent}) => (
-    <section className="section">
-        <header className="section__header">
-            <h2 className="section__title">{title}</h2>
+const Section = ({ title, body, extraHeaderContent }) => (
+    <StyledSection>
+        <SectionHeader>
+            <SectionTitle>{title}</SectionTitle>
             {extraHeaderContent}
-        </header>
-        <div className="section__body">
+        </SectionHeader>
+        <SectionBody>
             {body}
-        </div>
-    </section>
+        </SectionBody>
+    </StyledSection>
 );
 
 export default Section;
